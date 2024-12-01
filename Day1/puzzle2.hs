768e3e07593fb29bbebb44dcefde169a2822c2c1
Daniele Fucini <dfucini@gmail.com>
2024-12-01 12:05:04 +0100

    Simplify day 1, puzzle 2 code

--- Day1/puzzle2.hs
+++ Day1/puzzle2.hs
@@ -3,8 +3,7 @@ import System.IO
 
 similarityScore :: [Int] -> [Int] -> Int
 similarityScore xs ys = let elemsY = [ (head y, length y) | y <- (group . sort) ys ]
-                            xy = [ (x, snd y) | x <- xs, y <- elemsY, x == fst y ]
-                        in sum [ uncurry (*) x | x <- xy ]
+                        in  sum [ x * snd y | x <- xs, y <- elemsY, x == fst y ]
 
 main = do
     contents <- lines <$> readFile "day1.txt"
